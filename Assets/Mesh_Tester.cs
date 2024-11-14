76fcb6e7860e6065a30aeede393a7527976e1ffd
Baptiste Dulauroy <dulauroybaptiste@gmail.com>
2024-11-14 12:54:59 +0100

    Commit Batman Pathfinding
    Same

---
+++ Assets/Mesh_Tester.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Mesh_Tester : MonoBehaviour
+{
+
+    public Transform Target;
+
+    private NavMeshAgent _agent; 
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.destination = Target.position;
+    }
+
+}
