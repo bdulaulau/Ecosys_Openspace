ff56bb9d930542a52884a2f4fe35259eedc078d9
Baptiste Dulauroy <dulauroybaptiste@gmail.com>
2024-12-05 11:17:13 +0100

    Partie 2 avancé 05/12
    Spawn manuelle

--- Assets/Programme/Bureau_Test.cs
+++ Assets/Programme/Bureau_Test.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Bureau_Test : MonoBehaviour
 {
     public float proximitydistance = 1;
-    public Employee_Comportement employe;
+    public Employee_Comportement employee_comportement;
+    public GameObject Employee;
     private float timer = 0f;
+
 
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, employe.transform.position);
+        Employee = GameObject.Find("Employee");
+        float distance = Vector3.Distance(transform.position, Employee.transform.position);
         if (distance <= proximitydistance)
         {
             Debug.Log("Employé pas loin !");
             timer += Time.deltaTime;
             if (timer >= 1f)
             {
-                employe.TravailTest += 1;
+                employee_comportement.TravailTest += 1;
                 timer = 0f; //on réinitialise le timer
             }
         }
